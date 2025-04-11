7c981bc920118ca88ace0008312252301c8f6474
Ecembs <ecem.b@yeksoftware.com>
2025-04-12 01:56:32 +0300

    I've added the firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:food_delivery_app/core/routes/app_router.dart';
 import 'package:food_delivery_app/utils/theme/app_theme.dart';
 import 'package:provider/provider.dart';
-import 'core/enums/local_manager_keys.dart';
+import 'firebase_options.dart';
 import 'services/local_manager.dart';
 import 'view/base_view/base_view.dart';
 
 void main() async {
 WidgetsFlutterBinding.ensureInitialized();  
 await LocalManager.preferencesInit();
+
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
 runApp(ChangeNotifierProvider(
   create: (_) => AppTheme(),
   child: BaseView()));
